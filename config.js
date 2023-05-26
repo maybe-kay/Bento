8c7d403a8784ea81b6931a683b550206678afda5
maybe-kay <kay@kp2o.xyz>
2023-05-26 22:16:00 +0200

    modifié :         config.js

--- config.js
+++ config.js
@@ -11,16 +11,16 @@ const CONFIG = {
 	// └─┘┴ ┴└─┘┴└─┘└─┘
 
 	// General
-	name: 'John',
+	name: 'Irene',
 	imageBackground: false,
 	openInNewTab: true,
 	twelveHourFormat: false,
 
 	// Greetings
-	greetingMorning: 'Good morning!',
-	greetingAfternoon: 'Good afternoon,',
-	greetingEvening: 'Good evening,',
-	greetingNight: 'Go to Sleep!',
+	greetingMorning: 'Bonjour',
+	greetingAfternoon: 'Bonjour,',
+	greetingEvening: 'Bonsoir,',
+	greetingNight: 'Vas dormir!',
 
 	// Layout
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
